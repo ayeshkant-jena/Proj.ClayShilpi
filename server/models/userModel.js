71c0fbbb54398770706c4a51cc3b0a64cdf24254
Ayeshkant Jena <adeveloper1203@gmail.com>
2025-06-10 16:59:44 +0530

    working on backend part

---
+++ server/models/userModel.js
@@ -0,0 +1,19 @@
+const Users =  mongoose.model('Users',{
+  name:{
+    type:String,
+  },
+  email:{
+    type:String,
+    unique:true,
+  },
+  password:{
+    type:String,
+  },
+  cartData:{
+    type:Object,
+  },
+  date:{
+    type:Date,
+    default:Date.now,
+  }
+})
