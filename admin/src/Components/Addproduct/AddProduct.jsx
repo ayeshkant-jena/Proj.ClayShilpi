709e619f6a2bbee58ba8b0823d1e7970e57e7dcc
Ayeshkant Jena <adeveloper1203@gmail.com>
2025-06-19 15:18:45 +0530

    Changes in admin panel

--- admin/src/Components/Addproduct/AddProduct.jsx
+++ admin/src/Components/Addproduct/AddProduct.jsx
@@ -27,7 +27,7 @@ const AddProduct = ()=>{
         let formData = new FormData();
         formData.append('product',image);
 
-        await fetch('http:localhost:4000/upload',{
+        await fetch('http://localhost:4000/upload',{
             method:'POST',
             headers:{
                 Accept:'application/json',
