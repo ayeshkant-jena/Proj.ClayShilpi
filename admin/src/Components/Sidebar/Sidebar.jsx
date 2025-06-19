2875ee31128d4242588ea4b207e15549f83d0bdd
Ayeshkant Jena <adeveloper1203@gmail.com>
2025-06-19 17:18:52 +0530

    working on admin panel

--- admin/src/Components/Sidebar/Sidebar.jsx
+++ admin/src/Components/Sidebar/Sidebar.jsx
@@ -7,18 +7,27 @@ import list_product_icon from '../../assets/product_list.svg'
 const Sidebar =  ()=>{
     return(
         <div className="sidebar">
+            <Link to={'/dashboard'} style={{textDecoration:"none"}}>
+                <div className="sidebar-item">
+                    <img src={list_product_icon} alt="" />
+                    <p>Dashboard</p>
+                </div>
+            </Link>
+            <hr/>
             <Link to={'/addproduct'} style={{textDecoration:"none"}}>
                 <div className="sidebar-item">
                     <img src={add_product_icon} alt="" />
                     <p>Add Product</p>
                 </div>
             </Link>
+            <hr/>
             <Link to={'/listproduct'} style={{textDecoration:"none"}}>
                 <div className="sidebar-item">
                     <img src={list_product_icon} alt="" />
                     <p>Product List</p>
                 </div>
             </Link>
+            <hr/>
         </div>
     )
 }
